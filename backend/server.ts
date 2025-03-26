e66aed9ad9502172fa9321e2abf84270e0304c99
Randy Esplin <randy.esplin@ecinternet.com>
2025-03-26 13:49:34 -0700

    Add backend

---
+++ backend/server.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({ message: "Hello from the backend!" });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
